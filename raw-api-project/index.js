fd4d9e8c4977f6a9d8e91fe790a81854a4906910
bindubritto <sabbir.ruet.cse@gmail.com>
2021-08-09 11:15:24 +0600

    Created basic server

--- raw-api-project/index.js
+++ raw-api-project/index.js
@@ -1,1 +1,33 @@
-console.log("Hello from node js");
+
+
+// dependencies
+const http = require('http');
+const url = require('url');
+
+// app object
+const app = {};
+
+// config
+app.config = {
+    port: 3000
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`Listening to port ${app.config.port}`);
+    });
+}
+
+// handle req, res
+app.handleReqRes = (req, res) => {
+
+    // request handle
+
+    // response handle
+    res.end('Hello World');
+}
+
+// Start server
+app.createServer();
