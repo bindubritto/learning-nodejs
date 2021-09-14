1badb246f631ae8bfee707607711baa54561093e
bindubritto <sabbir.ruet.cse@gmail.com>
2021-09-14 10:33:08 +0600

    route guard implemented

--- raw-api-project/routes.js
+++ raw-api-project/routes.js
@@ -1,11 +1,13 @@
 // dependencies
 
 const { sampleHandler } = require('./handlers/routeHandlers/sampleHandler');
+const { tokenHandler } = require('./handlers/routeHandlers/tokenHandlers');
 const { userHandler } = require('./handlers/routeHandlers/userHandler');
 
 const routes = {
     sample: sampleHandler,
     user: userHandler,
+    token: tokenHandler,
 };
 
 module.exports = routes;
